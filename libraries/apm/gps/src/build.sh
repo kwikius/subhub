ff1bc04739ca3490b3661c8875850e5f0f7fcffb
skyscraper <kwikius@yahoo.com>
2016-04-15 10:48:13 +0100

    remove local build script

--- libraries/apm/gps/src/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-## exports to build subhub
-export TOOLCHAIN_PREFIX="/opt/gcc-arm-none-eabi-4_7-2013q2/"
-export TOOLCHAIN_GCC_VERSION="4.7.4"
-export QUAN_INCLUDE_PATH="/home/andy/website/quan-trunk"
-export STM32_STD_PERIPH_LIB_DIR="/opt/stm32f0/STM32F0xx_StdPeriph_Lib_V1.1.0/Libraries/"
-make
