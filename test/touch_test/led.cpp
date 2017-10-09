070a426999ff173ad238ef9570518a77d89d8483
Andy Little <kwikius@yahoo.com>
2017-10-09 23:30:50 +0100

    update led for discovery board

--- test/touch_test/led.cpp
+++ test/touch_test/led.cpp
@@ -9,11 +9,12 @@ namespace {
   /* on STM32F0 discovery board  
      LED3 is on PC9
      LED4 is on PC8
+     but use another pin to put led on
   */
 
 //  for subhub board Use Servo1 on PA5 TIM2_CH1 as LED
 #if defined (QUAN_STM32F0_DISCOVERY_BOARD)
-   typedef quan::mcu::pin<gpioc,9> led1;
+   typedef quan::mcu::pin<gpioa,11> led1;
 #else
    typedef quan::mcu::pin<gpioa,5> led1;
 #endif
@@ -22,12 +23,20 @@ namespace {
 
 void led::on()
 {
-  set<led1>();
+#if defined (QUAN_STM32F0_DISCOVERY_BOARD)
+   clear<led1>();
+#else
+   set<led1>();
+#endif
 }
 
 void led::off()
 {
-  clear<led1>();
+#if defined (QUAN_STM32F0_DISCOVERY_BOARD)
+   set<led1>();
+#else
+   clear<led1>();
+#endif
 }
 
 void led::complement()
@@ -41,10 +50,14 @@ void led::initialise()
    apply<
       led1
       , gpio::mode::output
-      , gpio::otype::push_pull
+      , gpio::otype::open_drain
       , gpio::pupd::none
       , gpio::ospeed::slow
+#if defined (QUAN_STM32F0_DISCOVERY_BOARD)
+      , gpio::ostate::high
+#else
       , gpio::ostate::low
+#endif
    >();
 
 };
